6e2a30a19d3f64dbb8fad2ee8640f4a06862a238
mengchao <344580221@qq.com>
2017-08-29 00:16:00 +0800

    merge2

--- tstub/src/test/java/com/reality/scout/tstub/Test.java
+++ tstub/src/test/java/com/reality/scout/tstub/Test.java
@@ -4,6 +4,6 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-		System.out.println("I am tstub-1");
+		System.out.println("I am tstub-1. Do you find your log?");
 	}
 }
